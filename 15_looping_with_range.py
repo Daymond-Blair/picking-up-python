cb96503e05c91773a8404c1a2c76ac1dab5c548e
Daymond Blair <dblair1987@live.com>
2017-12-27 00:47:20 -0800

    Add files via upload

---
+++ 15_looping_with_range.py
@@ -0,0 +1,12 @@
+# RANGE FUNCTION AND LOOPING
+
+for i in range(5):
+    print(i)
+    print("In loop!")
+
+print("\nEnd of the loop!!!")
+
+
+for x in range(3, 16):
+    print(x)
+    print("I think you're getting it!")
