13904c32fa97eb9fa1eadde5c41ad96f26ecfb62
Daymond Blair <daymond.blair@gmail.com>
2018-01-06 01:32:08 -0800

    you got this!

---
+++ 22_23_list_with_reverse_and_count.py
@@ -0,0 +1,35 @@
+# 22/23 Lists with Reverse and Count functions
+
+myList = ["me", "myself", 5]
+
+name = "Daymond" + "Blair"
+
+print(name)
+
+fullName = ["Daymond", "Blair"]
+
+print(fullName)
+
+cost = 5.56 + 12.45
+
+print(cost)
+
+shoppingList = [5.56, 12, "Amazon", "December 30", "Lain"]
+
+print(shoppingList)
+
+shoppingList.reverse()
+
+print(shoppingList)
+
+print(shoppingList.count("Lain"))
+
+shoppingCart = ["apple", "grapes", "ps4", "dreamcast", "grapes"]
+
+print(shoppingCart.count("grapes"))
+
+print(shoppingCart.count("dreamcast"))
+
+shoppingCart.reverse()
+
+print(shoppingCart)
