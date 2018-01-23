e86a623f1565303570eea6a8d8008689e808a47c
Daymond Blair <daymond.blair@gmail.com>
2018-01-23 01:27:32 -0800

    almost forgot

---
+++ 31_while_loop.py
@@ -0,0 +1,11 @@
+# 31 while loops....finally!
+
+name = input("What is your name: ")
+
+print("\nYou said your name was: " + name)
+
+num = 0
+while num < 10:
+    num = num + 1
+
+    print(name + str(num))
