4443da9e691ac6896eb86359dd9711375de7cbf0
Daymond Blair <daymond.blair@gmail.com>
2018-01-24 01:55:00 -0800

    easy

---
+++ 33_functions_creating_calling_indents.py
@@ -0,0 +1,14 @@
+# 33 Functions, creating, calling, indents
+
+
+def printSomeStuff():  # def is short for "define"
+    print("I just ate me some good ole chicken. ")
+    print("Now I got that itis. ")
+
+
+def printSomeMoreStuff():
+    print("**** Second.")
+
+
+printSomeStuff()
+printSomeMoreStuff()
